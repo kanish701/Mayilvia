3b87d270c1fa135f2b6d14e3672553a76f21451f
kanish701 <kanishkardharmalingam1223@gmail.com>
2026-03-19 20:42:56 +0530

    services,about updated with contact upadated

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-oxc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  build: {
+    chunkSizeWarningLimit: 1000,
   },
 });
